e92fcb3c715807374a4bc55d1bbb7500a5f43240
charles-henri poniard <c.poniard@gmail.com>
2018-06-13 17:32:06 +0200

    add anchors slow, resolve pbl banner

--- source/assets/javascript/all.js
+++ source/assets/javascript/all.js
@@ -1,10 +1,23 @@
-import "bootstrap";
-import "all.scss";
-import "navbar.js";
-
-
 import $ from "jquery";
 
 $(function(){
   console.log("I'm working with jQuery!")
 });
+
+
+$(document).ready(function() {
+ $('.js-scrollTo').on('click', function() { // Au clic sur un élément
+   var page = $(this).attr('href'); // Page cible
+   var speed = 1000; // Durée de l'animation (en ms)
+   $('html, body').animate( { scrollTop: $(anchor_1).offset().top }, speed ); // Go
+   return false;
+ });
+});
+
+
+import "bootstrap";
+import "all.scss";
+import "navbar.js";
+
+
+
